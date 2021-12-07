0807206e78e9179d03c1f922461b705e83386012
murilonerdx <mu-silva@outlook.com>
2021-12-07 17:49:46 -0300

    Exibição da imagem

--- src/com/murilonerdx/deteccao/OpenCVTest.java
+++ src/com/murilonerdx/deteccao/OpenCVTest.java
@@ -1,6 +1,10 @@
 package com.murilonerdx.deteccao;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
 
 public class OpenCVTest {
 
@@ -8,6 +12,12 @@ public class OpenCVTest {
             System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
             System.out.println(Core.VERSION);
 
+
+        Mat imagemColorida = imread("src/com/murilonerdx/deteccao/opencv_java.jpg", CV_LOAD_IMAGE_COLOR);
+
+        Util ut = new Util();
+
+        ut.mostraImagem(ut.convertMatToImage(imagemColorida));
 
     }
 }
